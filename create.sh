2dd1694049681b4b7e044d99c74822379362aa76
Evan Black <evanmblack@gmail.com>
2014-03-11 09:58:16 -0500

    Quick change

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@ touch $1.html $1.css
 echo "touch $1.html $1.css"
 git add $1.html
 echo "git add $1.html"
-git commit
+git commit -m "Adding page"
 git add $1.css
 echo "git add $1.css"
-git commit
+git commit -m "Adding style"
